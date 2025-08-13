0205aacd6077641cb1ff7771257da5ae182a465c
Asmita KC <theasmitakc@gmail.com>
2025-08-13 08:12:33 +0545

    changed fontsize

--- src/Common/Navbar/Navbar.jsx
+++ src/Common/Navbar/Navbar.jsx
@@ -4,14 +4,14 @@ const Navbar = () => {
   return (
     <div className='flex justify-between p-5 border border-gray-200 ' >
         <div >
-            <ul className='flex gap-10 text-xl'>
+            <ul className='flex gap-10 text-xm'>
                 <li className='font-black flex'>  <SquareArrowOutUpRight />asmitakc.com.np</li>
                 <li>Home</li>
                 <li>Blog</li>
                 <li>Resume</li>
             </ul>
         </div>
-        <div className='flex gap-5  text-xl'>
+        <div className='flex gap-5  text-xm'>
             <input className='border text-center' type="text" placeholder='Search sections...'
          />
             <Github  />
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,39 +4,27 @@ import { Mail, SquareArrowOutUpRight  } from 'lucide-react';
 const Home = () => {
   return (
     
-    <div className='flex  h-[100vh]'>
+    <div className='flex  h-[100vh] '>
         <div className='w-[20vw] border border-gray-100 pt-10 pl-5 '>
-            <h1 className='font-bold text-2xl ' >Sections</h1>
-            <ul className='text-xl mt-3 mr-2 '>
+            <h1 className='font-bold text-xl ' >Sections</h1>
+            <ul className='text-xm mt-3 mr-2 '>
                 <li className='mb-3 bg-gray-100 font-bold  border-0 rounded-xl p-2 cursor-pointer ' >Introduction</li>
                 <li className='mb-3 hover:bg-gray-100 hover:font-bold  border-0 rounded-xl p-2 cursor-pointer '>About Me</li>
                 <li className='mb-3 hover:bg-gray-100 hover:font-bold border-0 rounded-xl p-2 cursor-pointer '>Projects</li>
                 <li className='mb-3 hover:bg-gray-100 hover:font-bold  border-0 rounded-xl p-2 cursor-pointer '>Skills and Tools</li>
-                <a href='./experience' className='mb-3 hover:bg-gray-100 hover:font-bold  border-0 rounded-xl p-2 cursor-pointer ' >Experience</a>
+               <li className='mb-3 hover:bg-gray-100 hover:font-bold  border-0 rounded-xl p-2 cursor-pointer '> <a href='./experience' >Experience</a></li>
                 <li className='mb-3 hover:bg-gray-100 hover:font-bold  border-0 rounded-xl p-2 cursor-pointer '>Education</li>
                 <li className='mb-3 hover:bg-gray-100 hover:font-bold  border-0 rounded-xl p-2 cursor-pointer '>Contact</li>
             </ul>
         </div>
-        <div className='border border-gray-100 w-full pl-20 pt-10'><h1 className='text-5xl  font-extrabold'>Asmita KC</h1>
-        <h1 className='text-3xl text-gray-500  font-extrabold mt-10'>"The mind is everything. What you think you become." - Buddha</h1>
-        <h1  className='text-xl font-bold mt-10' >I am a Computer Engineer with a increasing passion in web developemnt with expertise in HTML, CSS, JavaScript and React creating responsive, user-friendly and modern websites.</h1>
+        <div className='border border-gray-100 w-full pl-20 pt-8'><h1 className='text-3xl  font-extrabold'>Asmita KC</h1>
+        <h1 className='text-2xl text-gray-500  font-extrabold mt-8'>"The mind is everything. What you think you become." - Buddha</h1>
+        <h1  className='text-xm mt-10' >I am a Computer Engineer with a increasing passion in web development with expertise in HTML, CSS, JavaScript and React creating responsive, user-friendly and modern websites.</h1>
         <div className='mt-5 flex  '>
-            <button className='border flex px-6 py-2 rounded-xl text-xl text-white bg-purple-500'>Get Resume  <SquareArrowOutUpRight /></button>
+            <button className='border flex px-3 py-2 rounded-xl text-xm text-white bg-purple-500'>Get Resume  <SquareArrowOutUpRight /></button>
           
             
-            <button  className=' flex hover:bg-gray-100 px-6 py-2 ml-5 rounded-xl text-xl '>  <Mail/>Send Email</button></div>
-           
-            <h1 className='font-bold text-2xl mt-10' >Welcome.js</h1>
-            <div className='bg-black mr-20 text-white h-[30vh] border-0 rounded-xl'>
-           <p className='p-10 text-xl text-gray-200'>  // app/api/welcome/route.js</p>
-           <span></span>
-{/* import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json({ message: "Welcome to my portfolio!" }, { status: 200 });
-} */}
-
-            </div>
+            <button  className=' flex hover:bg-gray-100 px-3 py-2 ml-5 rounded-xl text-xm '>  <Mail/>Send Email</button></div>
         </div>
        
     </div>
